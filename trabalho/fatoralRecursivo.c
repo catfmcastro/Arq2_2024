d60bdd776c6560f8b9cbe5791f74b8856fbd08da
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-06-06 16:27:39 -0300

    init trabalho de avaliação de desempenho

---
+++ trabalho/fatoralRecursivo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fatorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * fatorial(n - 1);
+    }
+}
+
+
+int main () {
+    int n;
+    printf("Digite um número: ");
+    scanf("%d", &n);
+    printf("Fatorial de %d: %d\n", n, fatorial(n));
+    return 0;
+}
